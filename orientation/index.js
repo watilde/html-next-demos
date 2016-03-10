21cfad1440875e34bab025160faf46f4499d90a8
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-03-10 18:20:57 +0100

    update orientation

--- orientation/index.js
+++ orientation/index.js
@@ -5,5 +5,8 @@ window.addEventListener("deviceorientation", function (e) {
   console.log('beta: ', beta);
   console.log('gamma: ', gamma);
   console.log('alpha: ', alpha);
+  document.getElementById("beta").innerHTML = beta;
+  document.getElementById("gamma").innerHTML = gamma;
+  document.getElementById("alpha").innerHTML = alpha;
   document.getElementById("orientation").style.translate3d = '(' + beta + ',' + gamma + ',' + alpha + ')';
 });
