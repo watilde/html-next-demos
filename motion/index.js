800f565079f97f2cd2075104259977f6703db2c1
Daijiro Wachi <daijiro.wachi@gmail.com>
2016-03-14 01:51:14 +0100

    add motion

---
+++ motion/index.js
@@ -0,0 +1,10 @@
+window.addEventListener("devicemotion", devicemotionHandler);
+
+function devicemotionHandler(event) {
+  var x = event.acceleration.x;
+  var y = event.acceleration.y;
+  var z = event.acceleration.z;
+  document.getElementById('x').innerHTML = 'x: ' + x;
+  document.getElementById('y').innerHTML = 'y: ' + y;
+  document.getElementById('x').innerHTML = 'z: ' + z;
+}
